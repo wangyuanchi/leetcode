95494f26743277a244b845835e134ea5682483f2
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2025-08-05 17:50:58 +0800

    Time: 12 ms (9.29%) | Memory: 19.2 MB (18.68%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,26 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Draw out the decision tree
+        # At each node, either we do a cooldown, or we buy/sell
+        # Remember that if we sell, we cannot buy the day after
+
+        # The key (day, buying) represents that I am at the state of being on that day,
+        # having the choice to buy or don't buy or sell or don't sell
         memo = {}
 
-        # Returns the maximum profit attainable given prices[i:]
-        def dp(i):
+        # Returns the max profit attainable given the state of the key
+        def dp(day, buying):
             # Base case
-            if i >= len(prices) - 1:
+            if day >= len(prices):
                 return 0
 
             # Memo
-            if i in memo:
-                return memo[i]
+            if (day, buying) in memo:
+                return memo[(day, buying)]
 
-            lowest = prices[i]
-            maxProfit = 0
-            for j in range(i, len(prices)):
-                if prices[j] <= lowest:
-                    lowest = prices[j]
-                else:
-                    maxProfit = max(maxProfit, prices[j] - lowest + dp(j + 2))
-            
-            memo[i] = maxProfit
-            return maxProfit
+            cooldown = dp(day + 1, buying) # Don't buy/sell
+            if buying:
+                memo[(day, buying)] = max(cooldown, - prices[day] + dp(day + 1, not buying))
+            else:
+                memo[(day, buying)] = max(cooldown, prices[day] + dp(day + 2, not buying))
 
-        return dp(0)
+            return memo[(day, buying)]
+
+        return dp(0, True)
