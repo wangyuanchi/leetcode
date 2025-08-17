a0d2e020e4c16cb9544f6d40464216ad41c6f785
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2025-08-17 15:20:21 +0800

    Time: 0 ms (100.00%) | Memory: 18 MB (8.29%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,17 +1,24 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        memo = {}
+        # M**n, M[0][0] is f(n+1)
+        # From examples, can see we want to return n+1th fibonacci number
+        def matrix_multiplication(A, B):
+            return [[A[0][0] * B[0][0] + A[0][1] * B[1][0],
+                    A[0][0] * B[0][1] + A[0][1] * B[1][1]],
+                    [A[1][0] * B[0][0] + A[1][1] * B[1][0],
+                    A[1][0] * B[0][1] + A[1][1] * B[1][1]]]
+        
+        def matrix_power(A, n):
+            if n == 1:
+                return A
 
-        def dp(n):
-            # Base case
-            if n == 1 or n == 2:
-                return n
+            res = matrix_power(A, n // 2)
+            res = matrix_multiplication(res, res)
+            if n % 2 == 1:
+                res = matrix_multiplication(res, A)
+            return res
             
-            # Memoization
-            if n in memo:
-                return memo[n]
-            
-            memo[n] = dp(n - 1) + dp(n - 2)
-            return memo[n]
 
-        return dp(n)
+        M = [[1, 1], [1, 0]]
+        res = matrix_power(M, n)
+        return res[0][0]
