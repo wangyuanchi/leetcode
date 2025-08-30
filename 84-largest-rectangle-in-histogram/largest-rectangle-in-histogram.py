6685d18e5ef27e3604c1af852d1a16ddfae4fdae
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2025-08-30 14:15:21 +0800

    Time: 191 ms (24.31%) | Memory: 35.3 MB (34.20%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.py
@@ -2,7 +2,7 @@ class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         # At least 1 height is completely in the largest rectangle (Proof by contradiction)
         # Check for all heights using a monotonically increasing stack
-        stack = [] # Element: (height, index, leftLimit)
+        stack = [] # Element: (height, leftLimit)
         maxArea = 0
 
         for i in range(len(heights) + 1):
@@ -15,9 +15,9 @@ class Solution:
             popLeftLimit = i
 
             while len(stack) > 0 and stack[-1][0] > height:
-                popHeight, popIndex, popLeftLimit = stack.pop()
+                popHeight, popLeftLimit = stack.pop()
                 maxArea = max(maxArea, popHeight * (i - popLeftLimit))
 
-            stack.append((height, i, popLeftLimit))
+            stack.append((height, popLeftLimit))
 
         return maxArea
