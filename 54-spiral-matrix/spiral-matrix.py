830af2664d9a755a283d61182445727880b63425
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2025-09-02 11:30:22 +0800

    Time: 0 ms (100.00%) | Memory: 17.8 MB (61.95%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.py
@@ -0,0 +1,34 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        # Unprocessed matrix cells are in those bounded by the 4 pointers
+        # [l, r] and [t, b]
+
+        l, r, t, b = 0, len(matrix[0]) - 1, 0, len(matrix) - 1
+        res = []
+
+        while True:
+            for i in range(l, r + 1):
+                res.append(matrix[t][i])
+            t += 1
+            if t > b:
+                break
+
+            for i in range(t, b + 1):
+                res.append(matrix[i][r])
+            r -= 1
+            if l > r:
+                break
+
+            for i in range(r, l - 1, -1):
+                res.append(matrix[b][i])
+            b -= 1
+            if t > b:
+                break
+
+            for i in range(b, t - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+            if l > r:
+                break
+
+        return res
