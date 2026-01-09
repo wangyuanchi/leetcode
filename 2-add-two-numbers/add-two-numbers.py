86bf4bcc3de83d3a7e3b503a526f3cdc3207b8c8
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2026-01-09 22:44:06 +0800

    Time: 9 ms (13.59%) | Memory: 19.2 MB (11.77%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -3,35 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        cur1, cur2 = l1, l2
-        dummy = result = ListNode()
-        carryOver = 0 # either 0 or 1
-        while cur1 and cur2:
-            curSum = cur1.val + cur2.val + carryOver
-            remainder = curSum % 10
-            carryOver = int((curSum - remainder) / 10)
-            dummy.next = ListNode(remainder)
-            dummy, cur1, cur2 = dummy.next, cur1.next, cur2.next
+        carry = 0
+        dummy = cur = ListNode()
+        
+        while l1 or l2 or carry:
+            l1_val = 0 if not l1 else l1.val
+            l2_val = 0 if not l2 else l2.val
 
-        # remaining nodes from l1 or l2
-        while cur1:
-            curSum = cur1.val + carryOver
-            remainder = curSum % 10
-            carryOver = int((curSum - remainder) / 10)
-            dummy.next = ListNode(remainder)
-            dummy, cur1 = dummy.next, cur1.next
+            sum = l1_val + l2_val + carry
+            carry = sum // 10
+            res = sum % 10
+            cur.next = ListNode(res)
 
-        while cur2:
-            curSum = cur2.val + carryOver
-            remainder = curSum % 10
-            carryOver = int((curSum - remainder) / 10)
-            dummy.next = ListNode(remainder)
-            dummy, cur2 = dummy.next, cur2.next
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+            cur = cur.next
 
-        # deal with last carryOver
-        if carryOver == 1:
-            dummy.next = ListNode(carryOver)
-
-        return result.next
+        return dummy.next
