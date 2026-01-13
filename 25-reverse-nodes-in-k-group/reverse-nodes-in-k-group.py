f34f9d00f64a8be06ebc4afe7384ec7cb346f314
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2026-01-14 00:06:56 +0800

    Time: 2 ms (36.89%) | Memory: 20.5 MB (6.03%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.py
@@ -6,46 +6,43 @@
 
 class Solution:
     def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        # Find the length of the linked list
-        length = 1
+        # Calculate the length of the linked list
+        total_length = 0
         cur = head
-        while cur.next:
+        while cur:
             cur = cur.next
-            length += 1
+            total_length += 1
+        reverse_count = total_length // k
 
-        slide_count = length // k
-        prev, cur = None, head
+        # Early return to prevent Null pointer
+        if reverse_count == 0:
+            return head
 
-        # The node that originally points to None after reversing but needs to point to some actual node
-        # The index represents the window this is in
-        missing_next = (0, head)
+        # Perform reverse_count reversals and reverse_count - 1 linking
+        cur = head
+        res, link = None, None
+        for i in range(reverse_count):
+            original_head, reversed_head, untouched_head = self.reverseSingleGroup(cur, k)
+            cur = untouched_head
 
-        res = None
-
-        # Slide the window slide_count times
-        for i in range(slide_count):
-            temp_head = cur
-
-            # Reverse linked list of size k, last node points to None
-            for j in range(k):
-                temp = cur.next
-                cur.next = prev
-                prev = cur
-                cur = temp
-
-            # Keep a pointer to the head for the result
+            # No linking for the first reversal
             if i == 0:
-                res = prev
+                res = reversed_head
+            else:
+                link.next = reversed_head
+            
+            link = original_head
 
-            # Update the missing next pointer from the previous window
-            if missing_next[0] + 1 == i:
-                missing_next[1].next = prev
-                missing_next = (i, temp_head)
-
-            prev = None
-
-        # Update the last missing_next
-        if cur:
-            missing_next[1].next = cur
+        link.next = cur
 
         return res
+       
+    def reverseSingleGroup(self, head: ListNode, k: int):
+        cur = head
+        prev = None
+        for _ in range(k):
+            temp = cur.next
+            cur.next = prev
+            prev = cur
+            cur = temp
+        return head, prev, cur
