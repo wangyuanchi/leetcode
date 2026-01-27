34311fddb3a0f0d8bdee3ec5425f13319cea5e02
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2026-01-27 11:21:05 +0800

    Time: 46 ms (60.75%) | Memory: 34.50 MB (22.23%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,36 +1,31 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        memo = {} # Optimization
         res = []
-        partition = []
+        builder = []
 
-        # [l, r)
-        def dfs(l, r):
-            # Base case
-            if r == len(s) + 1:
-                if l != len(s):
-                    return
-                for substring in partition:
-                    if not self.isPalindrome(substring):
-                        return
-                res.append(partition.copy())
+        def is_palindrome(substring):
+            l, r = 0, len(substring) - 1
+            while l < r:
+                if substring[l] != substring[r]:
+                    return False
+                l += 1
+                r -= 1
+            return True
+
+        # We are trying to find palindrome partitioning for s[i:]
+        def dfs(i):
+            if i == len(s):
+                res.append(builder.copy())
                 return
 
-            # Substring is in the partition
-            partition.append(s[l:r])
-            dfs(r, r + 1)
+            for j in range(i + 1, len(s) + 1):
+                if (i, j) not in memo:
+                    memo[(i, j)] = is_palindrome(s[i:j])
+                if memo[(i, j)]:
+                    builder.append(s[i:j])
+                    dfs(j)
+                    builder.pop()
 
-            # Substring is not in the partition
-            partition.pop()
-            dfs(l, r + 1)
-
-        dfs(0, 1)
+        dfs(0)
         return res
-    
-    def isPalindrome(self, substring):
-        l, r = 0, len(substring) - 1
-        while l < r:
-            if substring[l] != substring[r]:
-                return False
-            l += 1
-            r -= 1
-        return True
