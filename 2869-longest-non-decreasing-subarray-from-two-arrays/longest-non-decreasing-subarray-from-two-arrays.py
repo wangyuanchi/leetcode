da12d1af7c0393c0e4be7bce145f2aea0f7d82fa
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2026-02-07 20:51:21 +0800

    Time: 171 ms (55.13%) | Memory: 39.3 MB (61.97%) - LeetSync

--- 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
+++ 2869-longest-non-decreasing-subarray-from-two-arrays/longest-non-decreasing-subarray-from-two-arrays.py
@@ -1,22 +1,17 @@
 class Solution:
     def maxNonDecreasingLength(self, nums1: List[int], nums2: List[int]) -> int:
-        memo = {} # key: i, val
+        arr = [[1 for _ in range(len(nums1))] for _ in range(2)]
+        max_res = 1
 
-        def dp(i, val):
-            if (i, val) in memo:
-                return memo[(i, val)]
+        for i in range(1, len(nums1)):
+            res1 = 1 if nums1[i] < nums1[i - 1] else arr[0][i - 1] + 1
+            res2 = 1 if nums1[i] < nums2[i - 1] else arr[1][i - 1] + 1
+            arr[0][i] = max(res1, res2)
 
-            if i == 0:
-                return 1
+            res1 = 1 if nums2[i] < nums1[i - 1] else arr[0][i - 1] + 1
+            res2 = 1 if nums2[i] < nums2[i - 1] else arr[1][i - 1] + 1
+            arr[1][i] = max(res1, res2)
 
-            max_case_1 = dp(i - 1, nums1[i - 1]) + 1 if val >= nums1[i - 1] else 1
-            max_case_2 = dp(i - 1, nums2[i - 1]) + 1 if val >= nums2[i - 1] else 1
+            max_res = max(max_res, arr[0][i], arr[1][i])
 
-            memo[(i, val)] = max(max_case_1, max_case_2)
-            return memo[(i, val)]
-
-        res = 0
-        for i in range(len(nums1)):
-            res = max(res, dp(i, nums1[i]), dp(i, nums2[i]))
-        
-        return res
+        return max_res
