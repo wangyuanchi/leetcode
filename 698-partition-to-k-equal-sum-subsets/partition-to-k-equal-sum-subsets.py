46a7470acde274444812d04d02e892ebf274ebcb
wangyuanchi <46560637+wangyuanchi@users.noreply.github.com>
2026-08-23 00:18:22 +0800

    Time: 31 ms (50.08%) | Memory: 19.5 MB (52.58%) - LeetSync

--- 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
+++ 698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.py
@@ -1,7 +1,7 @@
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         nums.sort(reverse=True)
-        
+
         if sum(nums) % k:
             return False
 
@@ -18,7 +18,6 @@ class Solution:
             if i == len(nums):
                 return True
 
-            was_possible = False
             for j in range(k):
                 if sum_dict[j] + nums[i] > subset_sum:
                     continue
@@ -26,13 +25,13 @@ class Solution:
                 arr[j].append(nums[i])
                 sum_dict[j] += nums[i]
                 if backtrack(i + 1):
-                    was_possible = True
+                    return True
                 sum_dict[j] -= nums[i]
                 arr[j].pop()
 
                 if not arr[j]:
                     break
 
-            return was_possible
+            return False
 
         return backtrack(0)        
